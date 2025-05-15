bcd73cf1cc2c2d79af4c2617e7a0b5d8cc3d3558
ahsan-znok <rockersan2008@gmail.com>
2025-05-15 13:37:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("Latihan membuat website")
+st.title("Selamat datang di website informatika")
+st.image(
 st.write(
     "Latihan membuat website melalui Github dan Streamlit dengan menggunakan bahasa Python."
 )
