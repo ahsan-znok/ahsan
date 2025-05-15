81ea32407a71370bc03dc7fbad1080842844ffbd
ahsan-znok <rockersan2008@gmail.com>
2025-05-15 13:39:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Selamat datang di website informatika")
-st.image(
+st.image("IMG_8747.jpeg, width")
 st.write(
     "Latihan membuat website melalui Github dan Streamlit dengan menggunakan bahasa Python."
 )
