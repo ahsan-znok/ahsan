498c466bfbfe6d4fc8cad2c4cd858692e025e169
ahsan-znok <rockersan2008@gmail.com>
2025-05-15 14:30:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Selamat datang di website informatika")
-st.image("views/IMG_8747.jpeg", width=200)
+st.image("views/IMG_8747.jpeg",width=200)
 st.write("\n")
 st.subheader("Muhammad 'Mister' Ahsan")
 st.write(
